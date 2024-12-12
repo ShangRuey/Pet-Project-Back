34dca91659323b147362904bfdf88a893a7fe447
Shang <tom830522@gmail.com>
2024-12-12 21:25:45 +0800

    deploy：將cors 網域設成*

--- server.js
+++ server.js
@@ -339,7 +339,7 @@ app.get("/messages", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
